24c96969a760102ac1cf64cd5b8b5426b7864388
pitchawat <pitchawat.wiangnai@gmail.com>
2024-03-20 10:06:26 +0700

    init project

---
+++ app/src/main/java/com/playground/app/medium_articles_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.playground.app.medium_articles_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
